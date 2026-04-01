87b8f3404118362587fecd7ecebe6a429b5d6b2e
Hh <bunmenghong7@gmail.com>
2026-04-01 12:30:24 +0700

    add responding

---
+++ src/main/java/com/productmanagementsystem/common/response/Response.java
@@ -0,0 +1,55 @@
+package com.productmanagementsystem.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+
+
+@JsonPropertyOrder(value = {"code" , "message" , "description" , "timestamp" , "data"})
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class Response {
+    private String code;
+    private String message;
+    private  String description;
+    private Object data;
+    private Long timestamp;
+
+
+    private Response(String code, String message, String description, Object data) {
+        this.code = code;
+        this.message = message;
+        this.description = description;
+        this.data = data;
+        this.timestamp = System.currentTimeMillis();
+    }
+    private Response(String code, String message, String description) {
+        this.code = code;
+        this.message = message;
+        this.description = description;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+
+
+
+    public static Response success(String code, String message, String description , Object data) {
+        return new Response(code, message, description, data);
+    }
+
+    public static Response success(String code, String message, String description) {
+        return new Response(code, message, description);
+    }
+
+    public static Response error(String code, String message, String description) {
+        return new Response(code, message, description);
+    }
+
+    public static Response error(String code, String message ,  String description ,  Object data) {
+        return new Response(code, message, description, data);
+    }
+
+    public static Response badRequest(String code, String message, String description) {
+        return new Response("400", message, description);
+    }
+}
