93b7a83f4b51c8163be2b32724e6f6f2c23deae2
Hh <bunmenghong7@gmail.com>
2026-04-01 12:29:51 +0700

    add exception handling

---
+++ src/main/java/com/productmanagementsystem/exception/GlobalException.java
@@ -0,0 +1,64 @@
+package com.productmanagementsystem.exception;
+
+
+import com.productmanagementsystem.common.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<Response> handleDuplicateResourceException(DuplicateResourceException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Response.error("409" , "fail"  , e.getMessage()));
+    }
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Response> handleResourceNotFoundException(ResourceNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Response.error("404" , "fail"  , e.getMessage()));
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Response.error("500" , "fail" , e.getMessage()));
+    }
+
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Response> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String,String> errors = new HashMap();
+
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+
+            // insert into errors map
+            errors.put(fieldName,message);
+        });
+
+
+        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Response.error("400" , "fail"  ,"validation failed", errors));
+    }
+
+
+
+
+
+
+    @ExceptionHandler(UnprocessableEntityException.class)
+    public ResponseEntity<Response> handleUnprocessableEntityException(UnprocessableEntityException e){
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(Response.error("422","fail",e.getMessage()));
+    }
+
+}
